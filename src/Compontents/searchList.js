3865d898da79e13edd0f824be5fceaee97de26d1
federicotiralongo95@gmail.com <federicotiralongo95@gmail.com>
2022-10-14 15:57:25 +0200

    improviung

--- src/Compontents/searchList.js
+++ src/Compontents/searchList.js
@@ -1,45 +1,50 @@
-import Item from './Item'
-import { useContext } from 'react';
-import { ProductContext } from '../Context/ProductContext'
+import Item from "./Item";
+import { useContext, useEffect } from "react";
+import { ProductContext } from "../Context/ProductContext";
 import { useNavigate } from "react-router-dom";
-import "./search.css"
-
-
+import "./search.css";
 
 const SearchList = ({ filteredProducts }) => {
+  const { setSelectedProduct, selectedProduct } = useContext(ProductContext);
+  const naviagate = useNavigate();
+  const root = "https://front-test-api.herokuapp.com/api/product/";
 
-    const { selectProduct } = useContext(ProductContext)
-    const naviagate = useNavigate()
-    const filtered = filteredProducts.map((product) => {
-        return (
-            <>
-                <div onClick={() => {
-                    selectProduct(product)
-                    setTimeout(() => {
-                        naviagate("/details")
-                    }, 700);
-                }} >
-                    <Item
-                        imgUrl={product.imgUrl}
-                        brand={product.brand}
-                        model={product.model}
-                        price={product.price}
+  function chooseProduct(e) {
+    setSelectedProduct(e);
+    setTimeout(() => {
+      naviagate("/details");
+    }, 700);
+  }
 
-                    />
-                </div>
+  useEffect(() => {
 
-            </>
-        )
-    })
+    console.log(selectedProduct);
+  }, [selectedProduct]);
 
-
+  const filtered = filteredProducts.map((product) => {
     return (
-        <div className="listFlexContainer">
-            <div className="displayList">
-                {filtered}
-            </div>
+      <>
+        <div
+          onClick={() => {
+            chooseProduct(product);
+          }}
+        >
+          <Item
+            imgUrl={product.imgUrl}
+            brand={product.brand}
+            model={product.model}
+            price={product.price}
+          />
         </div>
-    )
-}
+      </>
+    );
+  });
 
-export default SearchList
+  return (
+    <div className="listFlexContainer">
+      <div className="displayList">{filtered}</div>
+    </div>
+  );
+};
+
+export default SearchList;
--- src/Context/ProductContext.js
+++ src/Context/ProductContext.js
@@ -1,64 +1,64 @@
-import { useEffect, createContext, useState } from 'react'
+import { useEffect, createContext, useState } from "react";
 
-
-
-
-
-export const ProductContext = createContext()
+export const ProductContext = createContext();
 
 const ProductsProvider = ({ children }) => {
+  const root = "https://front-test-api.herokuapp.com/";
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState({});
+  const [cart, setCart] = useState([]);
+  const [newProduct, setNewProduct] = useState({
+    id: "",
+    colorCode: "",
+    storageCode: "",
+  });
 
-    const root = "https://front-test-api.herokuapp.com/"
-    const [products, setProducts] = useState([])
-    const [selectedProduct, setSelectedProduct] = useState({})
-    const [cart, setCart] = useState([])
-    const [newProduct, setNewProduct] = useState({
-        id: "",
-        colorCode: "",
-        storageCode: ""
-    })
+  const endpoints = {
+    getAll: "api/product",
+    post: "api/cart",
+  };
 
-    const endpoints = {
-        getAll: "api/product",
-        post: "api/cart"
-    }
+  //   function chooseProduct(e) {
+  //     console.log(selectedProduct);
 
-    function selectProduct(e) {
-        setSelectedProduct(e)
-//         setTimeout(() => {
-// navigate("/details")
-//         }, 700);
-    }
+  //     setSelectedProduct(e);
 
-    function addCart(product) {
-        let newArr = [...cart]
-        newArr.push(product)
-        setCart(newArr)
-    }
+  //     console.log(selectedProduct);
 
-    useEffect(() => {
+  //   }
 
-        fetch(`${root}${endpoints.getAll}`)
-            .then((res) => res.json())
-            .then((data) => {
-                setProducts(data)
-                localStorage.setItem('allProducts', JSON.stringify(data))
-            })
-        console.log(newProduct);
-        console.log(cart);
-    }, [newProduct, cart])
+  function addCart(product) {
+    let newArr = [...cart];
+    newArr.push(product);
+    setCart(newArr);
+  }
 
+  useEffect(() => {
+    fetch(`${root}${endpoints.getAll}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        localStorage.setItem("allProducts", JSON.stringify(data));
+      });
+    console.log(newProduct, "newProduct");
+    console.log(cart, "cart");
+  }, [newProduct, cart]);
 
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        selectedProduct,
+        newProduct,
+        cart,
+        setSelectedProduct,
+        setNewProduct,
+        addCart,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
 
-
-
-    return (
-        <ProductContext.Provider value={{ products, selectedProduct, newProduct, cart, setNewProduct, selectProduct, addCart }}>
-            {children}
-        </ProductContext.Provider>
-    )
-}
-
-export { ProductsProvider }
-
-
+export { ProductsProvider };
